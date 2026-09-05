53c4992862c3819c6a6cdbbafeb98bc479829c4a
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2026-09-05 21:14:10 +0200

    Change date format to dd-mm-yyyy

--- includes/class-ber-reminder-mailer.php
+++ includes/class-ber-reminder-mailer.php
@@ -11,12 +11,14 @@ class BER_Reminder_Mailer {
 			__( 'Bardienst reminder voor %s', 'bar-email-reminder' ),
 			$reminder['name']
 		);
+		$date = DateTimeImmutable::createFromFormat( '!Y-m-d', $reminder['date'], wp_timezone() );
+		$date = $date ? $date->format( 'd-m-Y' ) : $reminder['date'];
 
 		$message = sprintf(
 			__( "Hallo %s,\n\nDit is je herinnering voor de bardienst bij The Victory.\n\nSleutel code: %s\nDatum bardienst: %s\n\nMet vriendelijke groet,\nThe Victory", 'bar-email-reminder' ),
 			$reminder['name'],
 			$reminder['code'],
-			$reminder['date']
+			$date
 		);
 
 		return wp_mail( BER_Reminder_Post_Type::get_emails( $reminder['email'] ), $subject, $message );
