0b4fc11d5208d2d7b63d77109c15959380bad3b9
Bjorn van der Neut <bvanderneut@independer.nl>
2026-09-11 09:23:14 +0200

    Remove tested up from php file

--- team-email-reminder.php
+++ team-email-reminder.php
@@ -4,7 +4,6 @@
  * Description: Manage dated reminders and send notification emails two days before their date.
  * Version: 1.0.0
  * Requires at least: 5.8
- * Tested up to: 7.1
  * Requires PHP: 7.4
  * license: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
